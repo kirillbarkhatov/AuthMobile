cf851d883229ff639ee025d206f993943276bf2d
kirill_barkhatov <k.s.barkhatov@gmail.com>
2025-03-04 00:02:55 +0300

    docker

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -186,3 +186,8 @@ if CACHE_ENABLED:
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 
 SMSAERO_API_KEY = os.getenv("SMSAERO_API_KEY")
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost',
+    'http://127.0.0.1',
+]
--- users/urls.py
+++ users/urls.py
@@ -11,20 +11,6 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("verify/", VerifyCodeView.as_view(), name="verify_code"),
     path("profile/", UserProfileView.as_view(), name="user_profile"),
-    path(
-        "logout/",
-        auth_views.LogoutView.as_view(next_page="authapp:index"),
-        name="logout",
-    ),
-    path(
-        "password_reset/",
-        auth_views.PasswordResetView.as_view(
-            template_name="password_reset/password_reset_form.html",
-            email_template_name="password_reset/password_reset_email.html",
-            success_url=reverse_lazy("users:password_reset_done"),
-        ),
-        name="password_reset",
-    ),
     path("send_sms/", SendSMSView.as_view(), name="send_sms"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
